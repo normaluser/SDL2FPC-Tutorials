4d678690a2b2d7ede87ed07789b5470d1d932f24
chunqian.shen <chunqian.shen@qq.com>
2023-07-22 13:49:31 +0800

    Fix

--- sdl2Shooter/shooter05/Shooter.App.pas
+++ sdl2Shooter/shooter05/Shooter.App.pas
@@ -16,7 +16,39 @@ uses
 var
   app: TApp;
 
+function createEntity: PEntity;
+
+procedure disposeEntity(e: PEntity);
+
 // ******************** implementation ********************
 implementation
+
+// 
+function createEntity: PEntity;
+var
+  e: PEntity;
+begin
+  New(e);
+  with e^ do
+  begin
+    x := 0.0;
+    y := 0.0;
+    dx := 0.0;
+    dy := 0.0;
+    h := 0;
+    w := 0;
+    health := false;
+    reload := 0;
+    Texture := Nil;
+    next := Nil;
+  end;
+  Result := e;
+end;
+
+// 
+procedure disposeEntity(e: PEntity);
+begin
+  Dispose(e);
+end;
 
 end.
